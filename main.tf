8410a509f651bf7952f13fee6a91195090d6a804
nirmalatatina1 <nirmalatatina1@gmail.com>
2024-06-17 13:12:33 +0530

    added

--- main.tf
+++ main.tf
@@ -117,7 +117,7 @@ EOF
 }
 
 resource "aws_instance" "web" {
-  ami             = "ami-04c913012f8977029"
+  ami             = "ami-08a0d1e16fc3f61ea"
   instance_type   = "t2.medium" 
   key_name        = var.key_name
   iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
--- vars/dev-west-2.tfvars
+++ vars/dev-west-2.tfvars
@@ -1,6 +1,6 @@
-aws_region = "ap-southeast-1"
+aws_region = "us-east-1"
 
-vpc_id = "vpc-0aca8658862e60802"
+vpc_id = "vpc-0a3291e7a7181193b"
 
 cidr_block = "172.31.0.0/16"
 
